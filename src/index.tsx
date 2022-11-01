b02ac11840bfbb6b469f3a273546aa671d7d16d9
Davi <davi.persantos@gmail.com>
2022-11-01 16:06:29 -0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Root } from './routes';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/Root/RootContainer.tsx
@@ -0,0 +1,14 @@
+import "./Root.css";
+
+export  function Root() {
+    return (
+      <>
+        <div>
+            <h1>Projeto MV</h1>
+        </div>
+      </>
+    );
+}
+
+
+export default Root;
